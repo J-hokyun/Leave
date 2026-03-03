5f9fae6eaaddc4c86fbd1cd45e5207d1e0285801
hokyun <wjdghrbs2468@naver.com>
2026-03-03 23:05:43 +0900

    Fix: [Webconfig] add DNS URL

--- backend/leave/src/main/java/project/leave/global/config/WebConfig.java
+++ backend/leave/src/main/java/project/leave/global/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000") // Flutter 웹이나 React 등 허용할 주소
+                .allowedOrigins("http://localhost:8080",
+                                "http://localhost:3000",
+                                "https://leavehistory.co.kr",     
+                                "https://www.leavehistory.co.kr", 
+                                "http://leavehistory.co.kr"       
+                                ) // Flutter 웹이나 React 등 허용할 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키/인증 정보 포함 허용
