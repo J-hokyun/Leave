2f86e85b4ffac6777e335cc3ad809fa1bee8286e
hokyun <wjdghrbs2468@naver.com>
2026-03-08 21:48:08 +0900

    chore:[server URL] seperate server url(dev, prod)

--- frontend/leave_application/lib/network/api_service.dart
+++ frontend/leave_application/lib/network/api_service.dart
@@ -1,13 +1,15 @@
 import 'package:dio/dio.dart';
 import 'auth_interceptor.dart';
+import 'package:flutter/foundation.dart';
 
 class ApiService {
   final Dio dio = Dio(); // 실제 통신을 담당하는 도구
 
   ApiService() {
     // 공통 주소 설정
-    // dio.options.baseUrl = 'http://localhost:8080'; -> 로컬용
-    dio.options.baseUrl = 'https://www.leavehistory.co.kr';
+    dio.options.baseUrl = kReleaseMode
+        ? 'https://www.leavehistory.co.kr'
+        : 'http://localhost:8080';
 
     // 시간 초과 설정 (선택)
     dio.options.connectTimeout = const Duration(seconds: 5);
