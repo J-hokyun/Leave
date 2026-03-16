90e2540d47bcea6f9258b4278afea502bd46966b
hokyun <wjdghrbs2468@naver.com>
2026-03-16 20:11:15 +0900

    Design: add enter in session expire message

--- frontend/leave_application/lib/network/auth_interceptor.dart
+++ frontend/leave_application/lib/network/auth_interceptor.dart
@@ -48,7 +48,7 @@ class AuthInterceptor extends Interceptor {
         }
         await AlertUtils.showAlert(
           currentContext,
-          "로그인이 만료되었습니다. 다시 로그인하여 주세요.",
+          "로그인이 만료되었습니다. \n다시 로그인하여 주세요.",
         );
 
         await storage.delete(key: 'ACCESS_TOKEN');
